1e04573515487f4baa5fb8d216b49237a9d07d98
Gabriela Feijo <feijo.gabriela@gmail.com>
2021-03-29 20:53:27 -0300

    Add first exercise - not finished

--- exercises/bloco_4/dia_4/exercicios-parte2.js
+++ exercises/bloco_4/dia_4/exercicios-parte2.js
@@ -1,1 +1,18 @@
 //Crie uma função que receba uma string e retorne true se for um palíndromo , ou false , se não for.Exemplo de palíndromo: arara . verificaPalindrome('arara') ;Retorno esperado: true verificaPalindrome('desenvolvimento') ; Retorno esperado: false
+let isPalindrome = true;
+let word = '';
+
+function verificaPalindrome() {
+   for(i=0; i < word; i+=1) {
+     word=n.length;
+    if(word[i] !== word.length){
+      return false;
+    }
+    word-=1;
+  }
+  return true;
+ }
+ 
+
+console.log(verificaPalindrome('arara'))
+console.log(verificaPalindrome('desenvolvimento'))
