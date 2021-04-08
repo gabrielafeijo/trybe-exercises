1f2cfab67f8c7f004b7f3792ccd6e22ecc3bdcdd
Gabriela Feijo <feijo.gabriela@gmail.com>
2021-04-08 19:28:17 -0300

    Finaliza exercícios

--- exercises/bloco_5_DOM/dia_3/exercises-5.3/script.js
+++ exercises/bloco_5_DOM/dia_3/exercises-5.3/script.js
@@ -160,23 +160,56 @@ Implemente uma função que adiciona uma legenda com cor para a tarefa criada no
 O parâmetro cor deverá ser utilizado como cor de fundo da <div> criada.
 O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks" .
 */
-function newTaskDiv(color) {
+function newTaskDiv(cor) {
 
-  let tasksContainer = document.querySelector('.my-tasks');
+  let taskContainer = document.querySelector('.my-tasks');
   let newTask = document.createElement('div');
 
   newTask.className = 'task';
-  newTask.style.backgroundColor = color;
-  tasksContainer.appendChild(newTask);
+  newTask.style.backgroundColor = cor;
+  taskContainer.appendChild(newTask);
 };
 
 newTaskDiv('purple');
 
 /*Exercício 9:
 Implemente uma função que adiciona um evento que ao clicar no elemento com a tag <div> referente a cor da sua tarefa, atribua a este elemento a classe task selected , ou seja, quando sua tarefa possuir a classe task selected ela estará selecionada.
-Ao clicar novamente no elemento a sua classe deverá voltar a ser somente task , ou seja, esta tarefa está deixando de ser uma tarefa selecionada.
+Ao clicar novamente no elemento a sua classe deverá voltar a ser somente task , ou seja, esta tarefa está deixando de ser uma tarefa selecionada.*/
+function setTasks() {
+  let myTasks = document.getElementsByClassName('task selected')
+  let getTasks = document.querySelector('.task');
 
+  myTasks.addEventListener('click', function(event){
+      if (getTasks.length === 0) {
+        event.target.className = 'task selected'
+      } else {
+        event.target.className = 'task';
+      }
+  })
+}
+setTasks();
+
+/*
 Exercício 10:
 Implemente uma função que adiciona um evento que ao clicar em um dia do mês no calendário, atribua a este dia a cor da legenda da sua tarefa selecionada.
-Ao clicar novamente no dia com a cor da legenda, a sua cor deverá voltar à configuração inicial rgb(119,119,119) .
+Ao clicar novamente no dia com a cor da legenda, a sua cor deverá voltar à configuração inicial rgb(119,119,119). Gabarito
 */
+
+ function setDayColor() {
+  let selectedTask = document.getElementsByClassName('task selected');
+  let days = document.querySelector('#days');
+  let taskDiv = document.querySelector('.task');
+  let taskColor = taskDiv.style.backgroundColor;
+  
+  days.addEventListener('click', function(event){
+    let eventTargetColor = event.target.style.color;
+    if (selectedTask.length > 0 && eventTargetColor !== taskColor) {
+      let color = selectedTask[0].style.backgroundColor;
+      event.target.style.color = color;
+    } else if (eventTargetColor === taskColor && selectedTask.length !== 0) {
+      event.target.style.color = 'rgb(119,119,119)';
+    }
+  });
+};
+
+setDayColor();
