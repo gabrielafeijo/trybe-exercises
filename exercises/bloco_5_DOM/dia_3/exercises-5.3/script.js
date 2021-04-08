f5c65a0174e431898bf308a04a6d43c2c47e4d34
Gabriela Feijo <feijo.gabriela@gmail.com>
2021-04-08 16:03:18 -0300

    Adiciona Exercícios 7 e 8

--- exercises/bloco_5_DOM/dia_3/exercises-5.3/script.js
+++ exercises/bloco_5_DOM/dia_3/exercises-5.3/script.js
@@ -96,7 +96,7 @@ function displayFridays(fridaysArray) {
   const getFridayButton = document.querySelector('#btn-friday');
   const getFridays = document.querySelectorAll('.friday')
   const backgroundColor = 'rgb(238,238,238)';
-  const setNewColor = 'red';
+  const setNewColor = 'salmon';
   const textFriday = "It's Friday, baby!"
 
   getFridayButton.addEventListener('click', function() {
@@ -140,11 +140,43 @@ aumenta();
 retorna();
 
 /* 
-Exercício 6:
-Implemente duas funções que criem um efeito de "zoom". Ao passar o ponteiro do mouse em um dia do mês no calendário, o texto desse dia deve aumentar e, quando o ponteiro do mouse sair do dia, o texto deve retornar ao tamanho original.
-Dica - Propriedade: event.target .
-
 Exercício 7:
 Implemente uma função que adiciona uma tarefa personalizada ao calendário. A função deve receber como parâmetro a string com o nome da tarefa (ex: "cozinhar") e criar dinamicamente um elemento com a tag <span> contendo a tarefa.
 O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks" .
+*/
+function newTaskSpan(task) {
+
+  let tasksContainer = document.querySelector('.my-tasks');
+  let taskName = document.createElement('span');
+
+  taskName.innerHTML = task;
+  tasksContainer.appendChild(taskName);
+};
+
+newTaskSpan('Projeto:');
+/*
+Exercício 8:
+Implemente uma função que adiciona uma legenda com cor para a tarefa criada no exercício anterior. Esta função deverá receber como parâmetro uma string ("cor") e criar dinamicamente um elemento de tag <div> com a classe task .
+O parâmetro cor deverá ser utilizado como cor de fundo da <div> criada.
+O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks" .
+*/
+function newTaskDiv(color) {
+
+  let tasksContainer = document.querySelector('.my-tasks');
+  let newTask = document.createElement('div');
+
+  newTask.className = 'task';
+  newTask.style.backgroundColor = color;
+  tasksContainer.appendChild(newTask);
+};
+
+newTaskDiv('purple');
+
+/*Exercício 9:
+Implemente uma função que adiciona um evento que ao clicar no elemento com a tag <div> referente a cor da sua tarefa, atribua a este elemento a classe task selected , ou seja, quando sua tarefa possuir a classe task selected ela estará selecionada.
+Ao clicar novamente no elemento a sua classe deverá voltar a ser somente task , ou seja, esta tarefa está deixando de ser uma tarefa selecionada.
+
+Exercício 10:
+Implemente uma função que adiciona um evento que ao clicar em um dia do mês no calendário, atribua a este dia a cor da legenda da sua tarefa selecionada.
+Ao clicar novamente no dia com a cor da legenda, a sua cor deverá voltar à configuração inicial rgb(119,119,119) .
 */
