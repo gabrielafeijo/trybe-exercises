b9920b09f6e9a7fe0fc7a935db8685133a11de06
Gabriela Feijo <feijo.gabriela@gmail.com>
2021-04-20 21:35:41 -0300

    atualiza arquivos

--- exercises/bloco_6_FORMS/dia_1/script.js
+++ exercises/bloco_6_FORMS/dia_1/script.js
@@ -1,10 +1,30 @@
-const states = document.getElementById('state');
-function createStateOptions() {
-  const brazilianStates = ['', 'AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
-  for (let index in brazilianStates) {
-    const createStates = document.createElement('option');
-    states.appendChild(createStates)
-    createStates.innerHTML = brazilianStates[index];
+const estados = ['', 'Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Distrito Federal','Espirito Santo','Goiás','Maranhão','Mato Grosso do Sul','Mato Grosso','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins']; 
+const getSelect = document.getElementById('estado');
+
+function getState () {
+  for (let index in estados) {
+    const createOption = document.createElement('option');
+    getSelect.appendChild(createOption);
+    createOption.innerHTML = estados[index];
   }
 }
-createStateOptions();
+getState();
+
+/* function renderCurriculum(event) {
+  event.preventDefault();
+  const formElements = document.querySelectorAll('input');
+  for (let index = 0; index < formElements.length; index += 1) {
+    if (formElements[index].type === 'radio' && !formElements[index].checked) {
+      continue;
+    }
+    const userInput = formElements[index].value;
+    const dataUser = document.querySelector('.form-data');
+    if (checkEmail() && checkData()) {
+      const div = document.createElement('div');
+      div.className = 'div-curriculum';
+      div.innerHTML = userInput;
+      dataUser.appendChild(div);
+    }
+  }
+}
+ */
