33d3ca0fd355f07d35ac722818a6f96d7b4331ef
Gabriela Feijo <feijo.gabriela@gmail.com>
2021-05-22 10:43:27 -0300

    Altera nome da pasta

---
+++ exercises/bloco_4/script.js
@@ -0,0 +1,29 @@
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+// Declare uma variável contendo uma string
+var nome = 'Gabriela';
+
+// Declare uma variável contendo um número dentro de uma string
+var ano = '2021';
+
+// Declare uma variável com a sua idade
+var idade = 34;
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var sobrenome = 'Feijó'
+var nomeCompleto = `${nome} ${sobrenome}`;
+
+// Coloque a seguinte frase em uma variável: It's time
+var frase = 'It\'s time';
+
+// Verifique o tipo da variável que contém o seu nome
+var verificarTipoNome = typeof idade;
+
+console.log(frase);
