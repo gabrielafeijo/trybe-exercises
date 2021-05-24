5e72fa0c57e0ec6b2d088cf7c47e73e58dc91c52
Gabriela Feijo <feijo.gabriela@gmail.com>
2021-05-23 21:40:26 -0300

    Primeiro commit de React

---
+++ exercises/bloco_11_Intro_React/jsx.js
@@ -0,0 +1,24 @@
+const nome = 'Jorge Maravilha';
+const element = <h1>Hello, {nome}</h1>;
+
+function nomeCompleto (nome, sobrenome) {
+  return `${nome} ${sobrenome}`;
+}
+
+const element = <h1>Hello, {nomeCompleto("Jorge", "Maravilha")}</h1>;
+
+//JSX
+function helloWorld (nome, sobrenome) {
+  return <h1>Hello, {`${nome} ${sobrenome}`}</h1>;
+}
+
+const container = <div>{helloWorld("Anna", "Silveira")}</div>;
+
+const nome = 'Jorge Maravilha';
+const classe = 'hello-class';
+const element = <h1 className={classe}>Hello, {nome}</h1>;
+
+//exercises - crie um elemento JSX que recebe o valor "Hello, JSX" de uma constante chamada textJSX , e o incorpora em uma tag h1 .
+
+const textJSX = 'Hello, JSX';
+const elementJSX = <h1 className={classe}> {textJSX}</h1>;
