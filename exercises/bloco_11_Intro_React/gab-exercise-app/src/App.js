2c96512c71e50ffbfe1740a3f219a46484b640f2
Gabriela Feijo <feijo.gabriela@gmail.com>
2021-05-23 22:45:05 -0300

    Adiciona exercício de fixação React

---
+++ exercises/bloco_11_Intro_React/gab-exercise-app/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import nameAndDescription from './Component'
+import './App.css';
+
+function App() {
+  return <nameAndDescription />;
+}
+
+export default App;
+
+//Não consegui puxar nesse formato, apenas como função!
---
+++ exercises/bloco_11_Intro_React/gab-exercise-app/src/Component.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class nameAndDescription extends Component {
+  render() {
+    return (
+    <div> 
+      <h1>Gabriela Feijó</h1>
+        <p> Gabriela é maravilhosa. Está super batalhando para dar conta desse curso, pois está bastante animada com tudo que irá fazer. Mágica!</p> 
+    </div>)
+  }
+}
+
+export default nameAndDescription;
